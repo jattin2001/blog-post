9638c6f4708f319ccb2613a842623ad5f0cc77a2
jattin2001 <103553560+jattin2001@users.noreply.github.com>
2023-02-12 23:07:58 +0530

    update

--- app.js
+++ app.js
@@ -92,6 +92,6 @@ app.get("/posts/:postId", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3002, function() {
+  console.log("Server started on port 3002");
 });
